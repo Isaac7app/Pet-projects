973307a1196e2dd56b48a2cf31869396a8d3e6e2
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-01-03 10:02:27 +0100

    made some promises

--- exercise_promises/exercisePromises.js
+++ exercise_promises/exercisePromises.js
@@ -23,3 +23,46 @@ const urls = [
 
 // #6) Change one of your urls above to make it incorrect and fail the promise
 // does your catch block handle it?
+
+
+
+
+// Creating my first promise
+const promise = new Promise((resolve,reject)=>{
+  if(true){
+    resolve('stuff worked');
+  } else{
+    reject('Error,it broke');
+  }
+})
+
+const promise2 = new Promise((resolve,reject)=>{
+  setTimeout(resolve,100,'HIII')
+})
+
+const promise3 = new Promise((resolve,reject)=>{
+  setTimeout(resolve,1000,'Pookie')
+})
+
+const promise4 = new Promise((resolve,reject)=>{
+  setTimeout(resolve,5000,'Is it me you are looking for?')
+})
+
+Promise.all([promise,promise2,promise3,promise4])
+.then(values =>{
+  console.log(values);
+})
+
+
+
+
+
+
+
+promise
+.then(result => result + '!')
+.then(result2 => {
+  throw Error
+  console.log(result2)
+})
+.catch(()=> console.log('errror!'))
