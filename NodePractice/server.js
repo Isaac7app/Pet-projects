e42bc23f5aae25647f1f7c6ffc69556b1e62a85a
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-01-05 11:18:46 +0100

    learning fs

--- NodePractice/server.js
+++ NodePractice/server.js
@@ -9,14 +9,14 @@ app.use(json())
 app.use(express.static(__dirname + '/public'))
 
 // app.get('/:id',(req,res)=>{
-//     //Mostly used req
-//     // req.query(?)
-//     // req.body
-//     // req.headers
-//     //req.params(/:id)
-//    res.status(404).send('Not found');
+//     Mostly used req
+//     req.query
+//     req.body
+//     req.headers
+//     console.log(req.params)
+//     req.params
+//     res.send('getting root')
 // })
-
 
 
 app.listen(3002);
