79ba1c1627a95b03a8f77e666457a6d0139dfa88
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-01-05 11:32:37 +0100

    appended a fs

--- NodePractice/script.js
+++ NodePractice/script.js
@@ -11,3 +11,14 @@ fs.readFile('./hello.txt', (err,data) =>{
 const file2 = fs.readFileSync('./hello.txt')
 console.log('Sync',file2.toString())
    
+fs.writeFile('./bye.txt','sad to see you go',err =>{
+    if(err){
+        console.log('erroorr')
+    }
+})
+
+fs.appendFile('./bye.txt', ' here i am added',err =>{
+    if(err){
+        console.log(err)
+    }
+})
