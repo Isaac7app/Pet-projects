2339c859bd90226b9f5e88b6d803cd84a8ebfb94
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-01-05 11:25:21 +0100

    made more fs practice

--- NodePractice/script.js
+++ NodePractice/script.js
@@ -4,6 +4,10 @@ fs.readFile('./hello.txt', (err,data) =>{
     if(err){
         console.log('erooorr!')
     }
-    console.log(data.toString());
+    console.log('Async',data.toString());
 
 })
+
+const file2 = fs.readFileSync('./hello.txt')
+console.log('Sync',file2.toString())
+   
