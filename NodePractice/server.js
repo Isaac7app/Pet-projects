b5ec4de6199518b717357bf19355bb23071e848f
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-01-05 10:12:42 +0100

    expanded on the express server

--- NodePractice/server.js
+++ NodePractice/server.js
@@ -1,10 +1,23 @@
-const express = require('express');
+const { urlencoded, json } = require('body-parser');
+const express = require('express')
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('getting root');
-})
+app.use(urlencoded({extended:false}))
+app.use(json())
+
+app.use(express.static(__dirname + '/public'))
+
+// app.get('/:id',(req,res)=>{
+//     //Mostly used req
+//     // req.query(?)
+//     // req.body
+//     // req.headers
+//     //req.params(/:id)
+//    res.status(404).send('Not found');
+// })
 
 
-app.listen(3005);
+
+app.listen(3002);
+
