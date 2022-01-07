4a43b343966d16340c5dc7c7e2d36f7c3dab5e7b
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-01-07 11:31:23 +0100

    made a post within the API

--- nodeApi/index.js
+++ nodeApi/index.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import usersRoutes from './routes/users.js';
+
 const app = express();
 const PORT = 5000;
 
 app.use(express.json());
 
+app.use('/users',usersRoutes);
+
 app.get('/',(req,res)=>{
-    console.log('[TEST TEST]!');
     res.send('hello from hompage');
 })
 
