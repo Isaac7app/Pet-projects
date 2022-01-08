f8d9e4fbe258d4b79dbc1394298a3019fd391d32
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-01-08 08:42:33 +0100

    updated the patch function on postman

--- nodeApi/routes/users.js
+++ nodeApi/routes/users.js
@@ -39,11 +39,13 @@ router.patch('/:id',(req,res)=>{
     const {id} = req.params;
     const {firstName,lastName,age} = req.body;
 
-    const user = user.find((user)=> user.id === id);
+    const user = users.find((user)=> user.id === id);
 
     if(firstName) user.firstName = firstName;
     if(lastName) user.lastName = lastName;
     if(age) user.age = age;
+
+    res.send(`user with the id:${id} has been updated`)
 })
 
 export default router;
