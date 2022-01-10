e615ae6fa2fdbeb847618aa67a205d8a2bc73c85
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-01-10 20:47:53 +0100

    working

--- Javascript_game/index.js
+++ Javascript_game/index.js
@@ -2,15 +2,15 @@ var character = document.getElementById("character");
 var block = document.getElementById("block");
 var counter=0;
 function jump(){
-    if(character.classList === "animate"){return}
+    if(character.classList == "animate"){return}
     character.classList.add("animate");
     setTimeout(function(){
         character.classList.remove("animate");
     },300);
 }
 var checkDead = setInterval(function() {
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
         block.style.animation = "none";
         alert("Game Over. score: "+Math.floor(counter/100));
