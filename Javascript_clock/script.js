c126dd27f0345870c66830d54994cb07c7baa4e7
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-01-12 20:13:23 +0100

    modified the clock setLnterval function

--- Javascript_clock/script.js
+++ Javascript_clock/script.js
@@ -1,8 +1,12 @@
 setInterval(setClock, 1000)
 
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
 const setClock = ()=>{
     const currentDate = new Date()
     const secondsRation = currentDate.getSeconds() /60;
-    const minutesRation = (secondsRation + currentDate.getMinutes() /60)
+    const minutesRation = (secondsRation + currentDate.getMinutes()) /60
     const hoursRation = currentDate.getHours() /60;
 }
