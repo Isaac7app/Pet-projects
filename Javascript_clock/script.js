f27d59365a48571e842c2db25cf180146453f524
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-01-12 12:05:10 +0100

    writing a functions to move the seconds and hours of the clock

--- Javascript_clock/script.js
+++ Javascript_clock/script.js
@@ -2,4 +2,7 @@ setInterval(setClock, 1000)
 
 const setClock = ()=>{
     const currentDate = new Date()
+    const secondsRation = currentDate.getSeconds() /60;
+    const minutesRation = (secondsRation + currentDate.getMinutes() /60)
+    const hoursRation = currentDate.getHours() /60;
 }
