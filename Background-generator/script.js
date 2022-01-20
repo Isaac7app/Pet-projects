cc90f2ed111ed6443ceee9889d612db56f6fd82b
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-01-20 13:48:38 +0100

    dom manupulation

---
+++ Background-generator/script.js
@@ -0,0 +1,12 @@
+const css = document.querySelector('h3');
+const color1 = document.querySelector('.color1');
+const color2 = document.querySelector('.color2');
+const body = document.getElementById('gradient');
+console.log(body);
+color1.addEventListener('input', ()=>{
+    
+})
+
+color2.addEventListener('input', ()=>{
+    console.log(color2.value);
+})
