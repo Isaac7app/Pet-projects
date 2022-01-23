5f3a964944db781b12190b1c8e97a8438935f3e9
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-01-23 18:43:09 +0100

    added a new component called Rank

--- facedetection/src/App.js
+++ facedetection/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/Navigation/ImageLinkForm/ImageLinkForm';
-
+import Rank from './components/Navigation/Rank/Rank'
 function App() {
   return (
     <div className="App">
      <Navigation />
      <ImageLinkForm />
+     <Rank />
     </div>
   );
 }
---
+++ facedetection/src/components/Navigation/Rank/Rank.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Rank = () => {
+    return (
+        <div >
+            <div className='white f3'>
+                {'Isaac, your current rank is...'}
+            </div>   
+        </div>
+      );
+}
+
+export default Rank;
+  
