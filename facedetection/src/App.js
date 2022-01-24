0dfb652ad752fa4dcf5f893b940a751aacee8e33
Isaac Appiagyei <ik.appiagyei@gmail.com>
2022-01-24 09:42:53 +0100

    adding a constructor

--- facedetection/src/App.js
+++ facedetection/src/App.js
@@ -1,24 +1,27 @@
 import './App.css';
-import Particles from "react-tsparticles";
 import Navigation from './components/Navigation/Navigation';
 import ImageLinkForm from './components/Navigation/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Navigation/Rank/Rank'
+import { Component } from 'react/cjs/react.production.min';
 
 
 
-
-
-
-
-
-function App() {
-  return (
-    <div className="App">
-     <Navigation />
-     <Rank />
-     <ImageLinkForm />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+       <Navigation />
+       <Rank />
+       <ImageLinkForm />
+      </div>
+    );
+  }
 }
 
 export default App;
